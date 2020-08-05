6d9b85ce4813c6420c704c9f33a7e66fd4cfe0e0
Dani Coleman <danicoleman00@gmail.com>
2020-08-05 09:08:54 -0600

    Add all initialize pieces

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,10 @@
 class Item
+  attr_reader :name,
+              :price
 
   def initialize(data)
-    # @argument = argument
+    @name  = data[:name]
+    @price = data[:price].delete('$').to_f
   end
 
 end
