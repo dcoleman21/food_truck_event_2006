1e97ed7824b15fb21c6813ac96a5e6eb5e066cef
Dani Coleman <danicoleman00@gmail.com>
2020-08-05 09:22:54 -0600

    Add check_stock and stock methods

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -7,5 +7,13 @@ class FoodTruck
     @inventory = Hash.new(0)
   end
 
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
+  end
+
 
 end
